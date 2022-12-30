494f01c3d5dee94f00f5f0f2dea990ccc5cb3417
hayteam5 <hayteam797@gmail.com>
2022-12-30 04:27:04 +0100

    add firebase app token

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {User} from "../../../shared/interfaces";
-import {Observable} from "rxjs";
+import {Observable, tap} from "rxjs";
+import {environment} from "../../../../environments/environment";
+import {FbAuthResponse} from "../../../../environments/interface";
 
 @Injectable()
 export class AuthService {
@@ -11,8 +13,11 @@ export class AuthService {
     return ''
   }
 
-  login(user: User): Observable<any> {
-    return this.http.post('', user)
+  login(user: User): Observable<FbAuthResponse> {
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap<any>(this.setToken)
+      )
   }
 
   logout() {
@@ -23,7 +28,7 @@ export class AuthService {
     return !!this.token
   }
 
-  private setToken() {
-
+  private setToken(response: FbAuthResponse) {
+    console.log(response.idToken)
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAjen7Eq44LLH2T6w_EDqjj5eY-4Il3WAo'
 };
