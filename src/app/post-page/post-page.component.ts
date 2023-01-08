8ee50f9aff83b69cb3f53160cf3cb8d3c5d2e700
hayteam5 <hayteam797@gmail.com>
2023-01-08 14:46:28 +0100

    add home page and posts rendering, add admin cabinet button from home page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Post} from "../shared/interfaces";
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostsService} from "../shared/posts.service";
+import {Observable, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  post$?: Observable<Post>
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    this.post$ = this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postService.getById(params['id'])
+        })
+      )
   }
 
 }
