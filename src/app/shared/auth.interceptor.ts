1d67cb7e6da959348c886af13f5505fa1cae0aaf
hayteam5 <hayteam797@gmail.com>
2023-01-17 01:57:57 +0100

    fix yml file

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -26,7 +26,7 @@ export class AuthInterceptor implements HttpInterceptor {
           console.log('[Interceptor Error]', error)
           if(error.status === 401) {
             this.auth.logout()
-            this.router.navigate(['/admin', 'login'], {
+            this.router.navigate(['/'], {
               queryParams: {
                 authFailed: true
               }
